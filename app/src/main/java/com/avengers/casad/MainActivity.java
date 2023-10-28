f92f8198061143f97228c3fbe266c15a8d2e9cab
Victor Pinheiro <victor.pinheiro.soft@gmail.com>
2023-10-28 19:42:31 -0300

    Initial commit
    Signed-off-by: victor4pinheiro <victor.pinheiro.soft@gmail.com>

---
+++ app/src/main/java/com/avengers/casad/MainActivity.java
@@ -0,0 +1,16 @@
+package com.avengers.casad;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
