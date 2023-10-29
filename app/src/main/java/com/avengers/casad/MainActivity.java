9112860f93f39634baf443db1587b5e7d2cf1f52
Matheus-Mapg <mapg.goes@gmail.com>
2023-10-29 00:42:52 -0300

    :sparkles: tela inicial feita

--- app/src/main/java/com/avengers/casad/MainActivity.java
+++ app/src/main/java/com/avengers/casad/MainActivity.java
@@ -1,15 +1,22 @@
 package com.avengers.casad;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.splashscreen.SplashScreen;
+import androidx.core.splashscreen.SplashScreenViewProvider;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
