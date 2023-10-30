d14612df257a108e540c9306dbf19ccdaeabf696
victoralv3s <victorpereira8889@gmail.com>
2023-10-30 13:48:42 -0300

    feat: Add strings, drawables and the ClientRegister activity

--- app/src/main/java/com/avengers/casad/MainActivity.java
+++ app/src/main/java/com/avengers/casad/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.client_register);
     }
 }
