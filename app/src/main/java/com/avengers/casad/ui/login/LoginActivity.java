bb9f450fc54c10329cf83d531cabea64e73b6727
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-10-30 20:54:34 -0300

    chore: update activites
    Signed-off-by: victor4pinheiro <victor.pinheiro.soft@gmail.com>

--- app/src/main/java/com/avengers/casad/ui/login/LoginActivity.java
+++ app/src/main/java/com/avengers/casad/ui/login/LoginActivity.java
@@ -1,11 +1,14 @@
 package com.avengers.casad.ui.login;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.avengers.casad.databinding.ActivityLoginBinding;
+import com.avengers.casad.ui.register.ClientRegisterActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -19,7 +22,13 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         final ImageButton backButton = binding.backButton;
+        final TextView registerRedirection = binding.registerRedirection;
 
         backButton.setOnClickListener(v -> finish());
+
+        registerRedirection.setOnClickListener(v -> {
+            Intent intent = new Intent(this, ClientRegisterActivity.class);
+            startActivity(intent);
+        });
     }
 }
